341f8d6edacc48c6ffb8a6581bb55e771b57b6de
oscar <oscar@Oscarito>
2015-02-22 22:07:52 -0600

    define structure ...ok read structure ... ok

---
+++ Proyect/main.cpp
@@ -0,0 +1,171 @@
+/* 
+ * File:   main.cpp
+ * Author: oscar
+ *
+ * Created on 22 de febrero de 2015, 06:30 PM
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+/* Declaracion de Funciones
+ */
+int printMenu(int);
+void defStruct(int);
+void addRecord();
+
+struct Field{
+	char name[15];
+	int integer;
+	int size;
+};
+
+istream& operator>>(istream& in, Field& field){
+	in >> field.name >> field.integer >> field.size;
+	return in;
+}
+ostream& operator<<(ostream& out, const Field& field){
+	out << field.name << field.integer << field.size;
+	return out;
+}
+
+int main(int argc, char** argv) {
+    int opcion;
+    do{
+        opcion = printMenu(0);
+        if(opcion==1)
+        {
+            cout << "Ingrese la cantidad de campos que tendra su estructura: ";
+            int fields;
+            cin >> fields;
+            defStruct(fields);
+        }
+        else if(opcion==2)
+        {
+            addRecord();
+	}
+	else if(opcion==4)
+        {
+            
+        }
+        else if(opcion==5)
+        {
+            
+	}
+	else if(opcion==6)
+        {
+            
+        }
+        else if(opcion==7)
+        {
+            
+        }
+        else if(opcion==8)
+        {
+            
+        }
+        else if(opcion==9)
+        {
+            cout << "Gracias por confiar en nosotros" << endl;
+        }
+        else 
+		{
+			cout << "Opcion seleccionada no existe" << endl;
+		}
+	}	
+	while(opcion != 9);
+    
+    return 0;
+}
+
+int printMenu(int index)
+{
+    int returnVal;
+    if(!index){
+        cout << "1) Definir Estructura " << endl;
+        cout << "2) Agregar Registro " << endl;
+        cout << "3) Listar Registros " << endl;
+        cout << "4) Buscar " << endl;
+        cout << "5) Borrar " << endl;
+        cout << "6) Modificar " << endl;
+        cout << "7) Compactar " << endl;
+        cout << "8) Activar Indice Lineal " << endl;
+        cout << "9) Salir " << endl;
+        cout << "Por favor seleccione una opcion:  " ;
+    }else{
+        cout << "1) Definir Estructura " << endl;
+        cout << "2) Agregar Registro " << endl;
+        cout << "3) Listar Registros " << endl;
+        cout << "4) Buscar " << endl;
+        cout << "5) Borrar " << endl;
+        cout << "6) Modificar " << endl;
+        cout << "7) Compactar " << endl;
+        cout << "8) Desactivar Indice Lineal " << endl;
+        cout << "9) Salir " << endl;
+        cout << "Por favor seleccione una opcion:  " ;
+    }
+    cin >> returnVal;
+    return returnVal;
+}
+
+void defStruct(int fields){
+    Field lstfields[fields];
+    int rrn=0;
+    for (int i = 0; i < fields; i++) {
+        cout << "Ingrese Nombre del campo N°"<<(i+1)<<": ";
+        cin >> lstfields[i].name;
+        cout << "Tipo 1 -> [Entero] | 0 -> [Texto]:";
+        cin >> lstfields[i].integer;
+        if (lstfields[i].integer) {
+            lstfields[i].size=4;
+        } else {
+            cout << "Ingrese Tamaño del texto: ";
+            cin >> lstfields[i].size;
+        }
+        rrn=rrn+lstfields[i].size;
+    }
+    int availist = -1;
+    int records=0;
+    ofstream erase;
+    erase.open("data.bin", std::ofstream::out | std::ofstream::trunc);
+    erase.close();
+    ofstream header("data.bin",ios::app | ios::binary); 
+    header.write(reinterpret_cast<char*>(&availist),sizeof(int));
+    header.write(reinterpret_cast<char*>(&fields),sizeof(int));
+    header.write(reinterpret_cast<char*>(&rrn),sizeof(int));
+    header.write(reinterpret_cast<char*>(&records),sizeof(int));
+    header.close();
+    ofstream out("data.bin", ios::app|ios::binary);
+    for (int i = 0; i < fields; i++) {
+        out.write(reinterpret_cast<char*>(&lstfields[i]),sizeof(Field));
+    }
+    out.close();
+
+}
+
+void addRecord(){
+    ifstream in("data.bin", ios::in|ios::binary);
+    int availist;
+    int records=0;
+    in.read((char*)&availist, sizeof(int));
+    int fields;
+    in.read((char*)&fields, sizeof(int));
+    int rrn;
+    in.read((char*)&rrn, sizeof(int));
+    char buffer[rrn];
+    Field arrFields[fields];
+    in.read((char*)&records, sizeof(int));
+    for (int i = 0; i < fields; i++) {
+        in.read(reinterpret_cast<char*>(&arrFields[i]),sizeof(Field));
+    }
+    
+
+
+}
